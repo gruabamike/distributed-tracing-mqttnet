805eba4113b572014a7c1c7039bdf2084148dfb1
gruabamike <gruabamike@gmx.at>
2025-02-07 16:55:28 +0100

    removed test projects

--- src/Services/IoT.Simulation.Client.StockMovement/Program.cs
+++
@@ -1,7 +0,0 @@
-using IoT.Simulation.Client.StockMovement;
-
-var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
-
-var host = builder.Build();
-host.Run();
